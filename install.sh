16f6b935448cefc9a2516073c446e550ecc5f18d
Ryan Dimsey <ryandimsey@gmail.com>
2023-05-11 16:22:31 +1000

    Inproved install script, added uninstall script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+cd $(dirname "$0")
 root=$PWD
 
 # libprom
@@ -11,3 +12,6 @@ cd $root/promhttp
 cmake .
 make
 sudo cp libpromhttp.so /usr/lib
+
+sudo cp -r $root/prom/include /usr/include/prom
+sudo cp $root/promhttp/include/promhttp.h /usr/include/prom
---
+++ uninstall.sh
@@ -0,0 +1,2 @@
+sudo rm /usr/lib/libprom.so /usr/lib/libpromhttp.so
+sudo rm -r /usr/include/prom
